7995e523074ab3c947c2d583bd841f0681e807fa
LeslieMay <zhangyujieswu@163.com>
2017-11-06 15:22:40 +0800

    add-mobile-tpl

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	var indexPage = (function(){
+		var domEvent = {
+			
+		}
+		var method = {
+			
+		}
+		var gainAjax = {
+			
+		}
+		var o = {
+			init:function(){
+				
+			}
+		}
+		return o;
+	})()
+		
+	indexPage.init();
+})
